0277c0c11483057c6167fd94663c5c14f6c80626
Rotem Adimor <54247736+rotemadimor@users.noreply.github.com>
2025-05-21 13:46:18 +0300

    Add files via upload

---
+++ Ex03.GarageLogic/GarageManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Ex03.GarageLogic
+{
+    public class GarageManager
+    {
+        private List<VehicleRecord> m_vehiclesInGarageRecords;
+        private const string k_DbFilePath = "Ex03.GarageLogic\\Vehicles.db";
+
+        public void LoadAllVehicles()
+        {
+            foreach (string line in File.ReadLines(k_DbFilePath))
+            {
+                if(line.Contains("*"))
+                {
+                    break;
+                }
+                Console.WriteLine(line);
+            }
+
+        }
+    }
+}
